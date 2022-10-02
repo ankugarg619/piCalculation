c3dfa67d4855a9336cffe2ae5bea2d326091966f
ankugarg619 <62594468+ankugarg619@users.noreply.github.com>
2022-10-02 14:24:55 +0530

    Addition of charts
    Change layout and add charts

--- index.js
+++ index.js
@@ -1,131 +1,48 @@
-const paymentTable = document.getElementsByClassName('paymentTable');
-
+const paymentTableClass = document.getElementsByClassName('paymentTable');
 const payTableData = document.getElementsByClassName('payTableData');
+const Calcbuttons = document.getElementById("calcButtons");
+const exportButtons = document.getElementById("exports");
+const paymentTableData = document.getElementById("tableToExport");
+const paymentSummary = document.getElementById("paymentSummary");
+
+let rateInpt = document.getElementById('interestRate');
+let amountInpt = document.getElementById('loanAmount');
+let termInpt = document.getElementById('loanTerm');
+
+function exportToCsv(){
+    var table2excel = new Table2Excel();
+    table2excel.export(paymentTableData);
+}
 
 
-paymentTable[0].addEventListener('click',function(){
-    paymentTable[0].classList.toggle('active');
+paymentTableClass[0].addEventListener('click',function(){
+    paymentTableClass[0].classList.toggle('active');
     payTableData[0].classList.toggle('active');
-})
+});
 
+class TableCSVExporter {
+    constructor(table, includeHeader=true) {
+      this.table = table;
+      this.rows = Array.from(table.querySelectorAll("tr"));
 
+      if (!includeHeader && this.rows[0].querySelectorAll("th").length){
+        this.rows.shift();
+      }
 
-
-function fillTable(){
-
-    let rateInpt = document.getElementById('interestRate');
-    let amountInpt = document.getElementById('loanAmount');
-    let termInpt = document.getElementById('loanTerm');
-
-    let rate = rateInpt.value;
-    let loanAmount = amountInpt.value;
-    let term = termInpt.value;
-
-    let paymentTable = document.getElementById("paymentTable");
-
-    term.va
-
-    function getFormatted(target){
-       return (new Intl.NumberFormat('en-IN',{style:"currency",currency:'INR'}).format(target));
-    }
-
-    function monthlyRate(target) {
-        return (target/1200)
-    }
-
-    function powmonthlyRate(target,n){
-        return (
-            (1+target)**n
-        )
-    }
-
-    function termMonths(target){
-        return target*12
-    }
-
-    function getMonthlyPayment(princiPal,monthRate,poweRate){
-        return (
-            (princiPal*monthRate*poweRate)/(poweRate-1)
-        )
-    }
-
-    function getMonthlyInterest(rate,principal){
-        return rate*principal
     }
 
 
-    function fillTable(){
+
+    static parseCell(tableCell){
+        let parsedValue = tableCell.textContent;
         
+        // Replace all double quotes with two double quotes
+        parsedValue = parsedValue.replace(/"/g,`""`);
+
+        // If value contains comma, new line or double quote, enclose between quote
+        parsedValue = /[",\n"]/.test(parsedValue) ? `"${parsedValue}"` : parsedValue;
+
+        return parsedValue;
     }
-    let outStandingBalance = loanAmount;
-    let paymentData = [];
-    let totalAmountPaid = 0.00;
+  }
 
-    let normalisedRate = monthlyRate(rate);
-    let normalisedTerm = termMonths(term);
-    let indexCal = powmonthlyRate(normalisedRate,normalisedTerm);
-    let monthlyPayment = getMonthlyPayment(outStandingBalance,normalisedRate,indexCal);
-
-    for (i=1;i<=normalisedTerm;i++){
-        
-        let interestPaid = normalisedRate*outStandingBalance;
-        let principalPaid = monthlyPayment - interestPaid;
-        totalAmountPaid += monthlyPayment;
-        outStandingBalance -= principalPaid;
-
-        let formatMonthPayment = getFormatted(monthlyPayment);
-        let formatPrincipalPaid = getFormatted(principalPaid);
-        let formatInterestPaid = getFormatted(interestPaid);
-        let formatTotalAmountPaid = getFormatted(totalAmountPaid);
-        let formatOutstandingBalance = getFormatted(outStandingBalance);
-
-        let monthData = [i,formatMonthPayment,formatInterestPaid,formatPrincipalPaid,formatTotalAmountPaid,formatOutstandingBalance];
-        paymentData.push(monthData);
-    }
-
-    let htmlTableOut = '';
-
-    for (j=0;j<normalisedTerm;j++){
-        let row = paymentData[j];
-        let rowString = '';
-        for(k=0;k<row.length;k++){
-            if(k===0){
-                rowString += `
-                <td>${row[k]}</td>
-                `;
-            }
-            else{
-                rowString += `<td>${row[k]}</td>`;
-            }
-        }
-
-        htmlTableOut += `<tr>${rowString}</tr>`;
-    }
-
-    paymentTable.innerHTML = `<table id="paymentTable">
-    <tr class="headerRow">
-      <th>Payment #</th>
-      <th>Monthly Payment</th>
-      <th>Interest paid</th>
-      <th>Principal paid</th>
-      <th>Total Payment</th>
-      <th>Balance</th>
-    </tr>
-    ${htmlTableOut}
-  </table>`;
-}
-
-
-function clearTable(){
-    let paymentTable = document.getElementById("paymentTable");
-    paymentTable.innerHTML = `<table id="paymentTable">
-    <tr class="headerRow">
-      <th>Payment #</th>
-      <th>Monthly Payment</th>
-      <th>Interest paid</th>
-      <th>Principal paid</th>
-      <th>Total Payment</th>
-      <th>Balance</th>
-    </tr>
-  </table>`;
-}
